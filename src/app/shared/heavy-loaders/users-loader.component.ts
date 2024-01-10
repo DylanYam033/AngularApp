880fed5977db21713ca417f59f25d145fb3f6fc9
DAYAMPUEZAN <auxiliar1.tecnologia-op@ienm.com.co>
2024-01-10 11:54:00 -0500

    new components

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: `<h1>Hola User</h1>`,
+})
+
+export class UsersLoaderComponent {
+  constructor() {}
+}
